7b07f5e155d804906e987b8a0e133a6dcbb95f6b
nadaalaa467 <n192004a@gmail.com>
2024-05-12 17:59:11 -0700

    api

--- app/Http/Controllers/ApI/DataController.php
+++ app/Http/Controllers/ApI/DataController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\ApI;
 
 use App\Models\CourseFiles;
+use App\Models\CourseRating;
+use App\Models\CourseTaken;
+use App\Models\CourseVideos;
+use App\Models\InstructionLevel;
+use App\Models\RoleUser;
+use App\Models\WithdrawRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
 use App\Models\Category;
 
 use App\Models\Course;
+use App\Models\Credit;
 use App\Models\Instructor;
+use App\Models\Role;
+use App\Models\Transaction;
+use App\Models\User;
 use PackageVersions\Installer;
 
 
@@ -17,6 +27,8 @@ class DataController extends Controller
 {
 
     // Blogs
+
+    // AllData
     function index(){
         $Blog = Blog::all();
         $data = [
@@ -27,6 +39,8 @@ class DataController extends Controller
         ];
         return response()->json($data);
     }
+
+    //ShowOne
 
     function show($id){
         $Blog = Blog::find($id);
@@ -40,6 +54,40 @@ class DataController extends Controller
         ];
         return response()->json($data);
     }
+
+    //Delete
+
+    function delete(Request $request){
+        $id = $request->id;
+        $Blog = Blog::find($id);
+
+        if($Blog){
+
+            $Blog->delete();
+        
+            $data = [
+                "msg" => "Deleted Successfully",
+                "Statud" => 200,
+                "data" =>  null
+    
+            ];
+            return response()->json($data);
+
+        }else{
+           
+        
+            $data = [
+                "msg" => "No Such Id",
+                "Statud" => 205,
+                "data" =>  null
+    
+            ];
+            return response()->json($data);
+        }
+       
+    }
+
+
 
     // Categories
 
@@ -67,6 +115,38 @@ class DataController extends Controller
         return response()->json($data);
     }
 
+     //Delete
+
+     function deleteCategories(Request $request){
+        $id = $request->id;
+        $Category = Category::find($id);
+
+        if($Category){
+
+            $Category->delete();
+        
+            $data = [
+                "msg" => "Deleted Successfully",
+                "Statud" => 200,
+                "data" =>  null
+    
+            ];
+            return response()->json($data);
+
+        }else{
+           
+        
+            $data = [
+                "msg" => "No Such Id",
+                "Statud" => 205,
+                "data" =>  null
+    
+            ];
+            return response()->json($data);
+        }
+       
+    }
+
   
     // Courses
 
@@ -92,7 +172,41 @@ class DataController extends Controller
         return response()->json($data);
     }
 
+     //Delete
 
+     function deleteCourses(Request $request){
+        $id = $request->id;
+        $Courses = Course::find($id);
+
+        if($Courses){
+
+            $Courses->delete();
+        
+            $data = [
+                "msg" => "Deleted Successfully",
+                "Statud" => 200,
+                "data" =>  null
+    
+            ];
+            return response()->json($data);
+
+        }else{
+           
+        
+            $data = [
+                "msg" => "No Such Id",
+                "Statud" => 205,
+                "data" =>  null
+    
+            ];
+            return response()->json($data);
+        }
+       
+    }
+
+
+
+    // CoursesFiles
     function CourseFile(){
         $CourseFile = CourseFiles::all();
         $data = [
@@ -115,10 +229,124 @@ class DataController extends Controller
         return response()->json($data);
     }
 
+    // CoursesRating
+    function CourseRating(){
+        $CourseRating = CourseRating::all();
+        $data = [
+            "msg" => "Return All Data",
+            "Statud" => 200,
+            "data" => $CourseRating
+
+        ];
+        return response()->json($data);
+    }
+
+    function ShowCourseRating($id){
+        $CourseRating = CourseRating::find($id);
+        $data = [
+            "msg" => "Return All Data",
+            "Statud" => 200,
+            "data" => $CourseRating
+
+        ];
+        return response()->json($data);
+    }
+
+       // CoursesTaken
+       function CourseTaken(){
+        $CourseTaken = CourseTaken::all();
+        $data = [
+            "msg" => "Return All Data",
+            "Statud" => 200,
+            "data" => $CourseTaken
+
+        ];
+        return response()->json($data);
+    }
+
+    function ShowCourseTaken($id){
+        $CourseTaken = CourseTaken::find($id);
+        $data = [
+            "msg" => "Return All Data",
+            "Statud" => 200,
+            "data" => $CourseTaken
+
+        ];
+        return response()->json($data);
+    }
 
 
-  
-        
+        // CoursesVideos
+        function CourseVideos(){
+            $CourseVideos = CourseVideos::all();
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $CourseVideos
+    
+            ];
+            return response()->json($data);
+        }
+    
+        function ShowCourseVideos($id){
+            $CourseVideos = CourseVideos::find($id);
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $CourseVideos
+    
+            ];
+            return response()->json($data);
+        }
+
+
+         // Credit
+         function Credit(){
+            $Credit = Credit::all();
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $Credit
+    
+            ];
+            return response()->json($data);
+        }
+    
+        function ShowCredit($id){
+            $Credit = Credit::find($id);
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $Credit
+    
+            ];
+            return response()->json($data);
+        }
+
+
+           // InstructionLevels
+           function InstructionLevel(){
+            $InstructionLevel = InstructionLevel::all();
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $InstructionLevel
+    
+            ];
+            return response()->json($data);
+        }
+
+        function ShowInstructionLevel($id){
+            $InstructionLevel = InstructionLevel::find($id);
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $InstructionLevel
+    
+            ];
+            return response()->json($data);
+        }
+   
   
     //Instructor
 
@@ -133,7 +361,6 @@ class DataController extends Controller
         return response()->json($data);
     }
 
-    //ShowInstructor
 
     function ShowInstructor ($id){
         $Instructor =Instructor::find($id);
@@ -146,5 +373,130 @@ class DataController extends Controller
         return response()->json($data);
     }
 
+
+       //Roles
+
+       function Role (){
+        $Role =Role::all();
+        $data = [
+            "msg" => "Return All Data",
+            "Statud" => 200,
+            "data" => $Role
+
+        ];
+        return response()->json($data);
+    }
+
+
+    function ShowRoles ($id){
+        $Role =Role::find($id);
+        $data = [
+            "msg" => "Return All Data",
+            "Statud" => 200,
+            "data" => $Role
+
+        ];
+        return response()->json($data);
+    }
+
+
+        //RolesUser
+
+        function RoleUser (){
+            $RoleUser =RoleUser::all();
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $RoleUser
+    
+            ];
+            return response()->json($data);
+        }
+    
+    
+        function ShowRolesUser ($id){
+            $RoleUser =RoleUser::find($id);
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $RoleUser
+    
+            ];
+            return response()->json($data);
+        }
+    
+          //Transaction
+
+          function Transaction (){
+            $Transaction =Transaction::all();
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $Transaction
+    
+            ];
+            return response()->json($data);
+        }
+    
+    
+        function ShowTransaction ($id){
+            $Transaction =Transaction::find($id);
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $Transaction
+    
+            ];
+            return response()->json($data);
+        }
+
+          //User
+
+          function User (){
+            $User =User::all();
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $User
+            ];
+            return response()->json($data);
+        }
+    
+    
+        function ShowUser ($id){
+            $User =User::find($id);
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $User
+    
+            ];
+            return response()->json($data);
+        }
+
+
+           //WithdrawRequest
+
+           function WithdrawRequest (){
+            $WithdrawRequest =WithdrawRequest::all();
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $WithdrawRequest
+            ];
+            return response()->json($data);
+        }
+    
+    
+        function ShowWithdrawRequest ($id){
+            $WithdrawRequest =WithdrawRequest::find($id);
+            $data = [
+                "msg" => "Return All Data",
+                "Statud" => 200,
+                "data" => $WithdrawRequest
+    
+            ];
+            return response()->json($data);
+        }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,11 +24,15 @@ Route::get('/allBlogs','API\DataController@index');
 
 Route::get('/showone/{id}','API\DataController@show');
 
+Route::post('/delete','API\DataController@delete');
+
 // Categories
 
 Route::get('/allcategory','API\DataController@Category');
 
 Route::get('/Showcategory/{id}','API\DataController@Showcategory');
+
+Route::post('/deleteCategories','API\DataController@deleteCategories');
 
 
 // Courses
@@ -35,12 +40,89 @@ Route::get('/allcourse','API\DataController@Course');
 
 Route::get('/ShowCourse/{id}','API\DataController@ShowCourse');
 
+Route::post('/deleteCourses','API\DataController@deleteCourses');
+
+// CoursesFiles
+
 Route::get('/allcourseFile','API\DataController@CourseFile');
 
 Route::get('/ShowCourseFile/{id}','API\DataController@ShowCourseFile');
 
+// CoursesRating
+
+Route::get('/allcourseRating','API\DataController@CourseRating');
+
+Route::get('/ShowCourseRating/{id}','API\DataController@ShowCourseRating');
+
+// CoursesTaken
+
+Route::get('/allcourseTaken','API\DataController@CourseTaken');
+
+Route::get('/ShowCourseTaken/{id}','API\DataController@ShowCourseTaken');
+
+// CoursesVideo
+
+Route::get('/allcourseVideos','API\DataController@CourseVideos');
+
+Route::get('/ShowCourseVideos/{id}','API\DataController@ShowCourseVideos');
+
+
+// Credit
+
+Route::get('/allCredit','API\DataController@Credit');
+
+Route::get('/ShowCredit/{id}','API\DataController@Credit');
+
+
+// InstructionLevels
+
+Route::get('/allInstructionLevel','API\DataController@InstructionLevel');
+
+Route::get('/ShowInstructionLevel/{id}','API\DataController@ShowInstructionLevel');
+
+// Instructor
 
 Route::get('/allinstructor','API\DataController@Instructor');
 
 
 Route::get('/ShowInstructor/{id}','API\DataController@ShowInstructor');
+
+
+// Role
+
+Route::get('/allRoles','API\DataController@Role');
+
+
+Route::get('/ShowRoles/{id}','API\DataController@ShowRoles');
+
+
+// RoleUser
+
+Route::get('/allRolesUser','API\DataController@RoleUser');
+
+
+Route::get('/ShowRolesUser/{id}','API\DataController@ShowRolesUser');
+
+
+// Transaction
+
+Route::get('/allTransaction','API\DataController@Transaction');
+
+
+Route::get('/ShowTransaction/{id}','API\DataController@ShowTransaction');
+
+
+// User
+
+Route::get('/allUser','API\DataController@User');
+
+
+Route::get('/ShowUser/{id}','API\DataController@ShowUser');
+
+
+// WithdrawRequest
+
+Route::get('/allWithdrawRequest','API\DataController@WithdrawRequest');
+
+
+Route::get('/ShowWithdrawRequest/{id}','API\DataController@ShowWithdrawRequest');
